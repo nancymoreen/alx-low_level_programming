d4964327a4a72dad1e032ce624fc680bce03c3f9
nancymoreen <nancymoreen@gmail.com>
2023-02-19 06:57:11 -0800

    Single digit numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - prints single digit numbers starting from 0
- * Return: Alway 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n <= 9; n++)
-		putchar(n);
+		putchar((n % 10) + '0');
 
 	putchar('\n');
 
