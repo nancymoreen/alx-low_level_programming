d368e0fa1550c7bb688e7c3a562666e9d7045f6a
nancymoreen <nancymoreen@gmail.com>
2023-02-27 08:31:22 -0800

    Prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input value
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+
+	for (; str[n] != '\0'; n++)
+	{
+		if ((n % 2) == 0)
+			_putchar(str[n]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
