e0ff444c484cea3a1b2878a9818f6093c0068ca3
nancymoreen <nancymoreen@gmail.com>
2023-03-01 23:39:03 -0800

    Encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: input value
+ * Return: n
+ */
+
+char *leet(char *n)
+{
+	int x;
+	int y;
+	char s1[] = "AaEeOoTtLl";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == s1[y])
+			{
+				n[x] = s2[y];
+			}
+		}
+	}
+	return (n);
+}
+
