fb2e65cb69319f8f4315cc54453c3e190f18f41a
nancymoreen <nancymoreen@gmail.com>
2023-03-11 07:04:51 -0800

    Prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 /**
- *
+ * main - prints the sum of two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Success 0, Error 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	unsigned int y, sum = 0;
+	char *c;
+
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			c = argv[x];
+
+			for (y = 0; y < strlen(c); y++)
+			{
+				if (c[y] < 48 || c[y] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+
+				sum += atoi(c);
+				c++;
+			}
+			printf("%d\n", sum);
+		}
+		else
+		{
+			printf("0\n");
+		}
+	}
+	return (0);
+}
