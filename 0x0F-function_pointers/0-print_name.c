edbdabf65c051063d366ad98361f127728641a0e
nancymoreen <nancymoreen@gmail.com>
2023-03-22 04:18:11 -0700

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: Name of the person
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
