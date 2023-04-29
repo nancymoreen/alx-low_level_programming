9d2ea41b24dd6a09e7221ff24c16149102272ec0
nancymoreen <nancymoreen@gmail.com>
2023-04-28 23:22:26 -0700

    elf header

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,31 +1,33 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text to the end of a file
- * @filename: Name of the file
- * @text_content: NULL terminated string to add at the end of the file
- * Return: 1 on success, -1 on failure
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
+ *
+ * Return: 1 on success and -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, b, c;
+	int fd, t, s = 0;
 
-	c = 0;
-
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
 	{
-		for (c = 0; text_content[c];)
-			c++;
+		while (text_content[s])
+			s++;
+		t = write(fd, text_content, s);
+		if (t != s)
+			return (-1);
 	}
-	a = open(filename, O_WRONLY, O_APPEND);
-	b = write(a, text_content, c);
 
-	if (a == -1 || b == -1)
-		return (-1);
+	close(fd);
 
-	close(a);
 	return (1);
 }
