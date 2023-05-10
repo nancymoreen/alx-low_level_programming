7dd91607ec4f2c2e4f21500f137a3a5b789a1057
nancymoreen <nancymoreen@gmail.com>
2023-05-09 19:09:41 -0700

    if/ele, loops, functions

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for x in range(97, 123): # ASCII Alphabet
+    if (chr(x) not in ['q' , 'e']:
+            print("{} ".format(chr(x)), end='')
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+def print_last_digit(number):
+    last_digit = number % 10
+    print("The last digit of", number, "is", last_digit)
+    return last_digit
