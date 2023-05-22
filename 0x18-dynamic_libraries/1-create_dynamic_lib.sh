7bc7da11803e59a589eab2a5d7b826c3094083a4
nancymoreen <nancymoreen@gmail.com>
2023-05-22 11:44:38 -0700

    Script for dynamic library creation

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -fPIC -shared -o liball.so *.c
+gcc -Wall -pedantic -Werror -Wextra -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
